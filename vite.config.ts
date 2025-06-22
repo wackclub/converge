b8330659aa1cb3da39fc669bb90f08bd2b672b8b
Mahad Kalam <mahadkalam1@proton.me>
2025-06-22 21:21:00 +0100

    buh-bye

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
+  plugins: [react()],
   publicDir: "./static",
   base: "./",
   css: {
