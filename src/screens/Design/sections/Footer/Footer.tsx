3292aa5fe2fe1d9d8f235008cf4ad145217de48e
Mahad Kalam <mahadkalam1@proton.me>
2025-06-24 19:48:08 +0100

    s

--- src/screens/Design/sections/Footer/Footer.tsx
+++ src/screens/Design/sections/Footer/Footer.tsx
@@ -11,6 +11,8 @@ export const Footer = (): JSX.Element => {
                         <h3 className="[font-family:'Chivo',Helvetica] font-semibold text-2xl mb-2">CONVERGE</h3>
                         <p className="[font-family:'Chivo',Helvetica] font-light text-sm opacity-80">
                             MADE WITH {"<3"} BY <a href="https://skyfall.dev/" className="underline" target="_blank">SKYFALL</a>
+                            <br />
+                            <span className="hidden sm:block">(and no, <a href="https://www.figma.com/design/mbzJHCEW33WlDGMNRKFWrE/Converge?node-id=0-1&t=l6IEgooSGG0ZZiCv-1" className="underline">the site isn't AI generated</a>, that's just my artstyle)</span>
                         </p>
                     </div>
 
